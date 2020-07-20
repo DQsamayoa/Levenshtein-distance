6116bc55875a3ed73514d4b0550f026479c75fdb
Victor Samayoa <vasamayoa@gmail.com>
2020-07-19 23:39:14 -0500

    Added code to import json file with characters for french, english and spanish.

--- levenshtein/__init__.py
+++ levenshtein/__init__.py
@@ -2,3 +2,17 @@ __all__ = ['levenshtein.py']
 
 # Defining the modules to import
 from .levenshtein import *
+
+# Defining variables to import
+import os
+import json
+
+this_dir, this_filename = os.path.split(__file__)
+data_path = os.path.join(this_dir, "data", "alphabet.json")
+
+with open(data_path) as json_file:
+    alphabet_json = json.load(json_file)
+
+del data_path
+del this_dir
+del this_filename
